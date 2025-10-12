c2b549e573edaf1661322b0fc66fdcceb93d19d4
Michael <mail@mwdenis.com>
2025-10-12 19:44:05 -0400

    Added matrix bank to testbench

--- src/control_unit.vhd
+++ src/control_unit.vhd
@@ -2,159 +2,172 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library tensors;
+use tensors.tensors.all;
+
 entity control_unit is
-    port(
-        i_clock                 : in  std_logic;
-        i_reset                 : in  std_logic; -- synchronous, active-high
-        -- inputs
-        i_cu_start              : in  std_logic;
-        o_cu_done               : out std_logic;
-        --Program Counter inteface
-        i_program_count         : in  std_logic_vector(31 downto 0);
-        o_program_counter_ready : out std_logic;
-        o_new_program_count     : out std_logic_vector(31 downto 0);
-        o_jump_flag             : out std_logic;
-        --Instruction memory interface
-        o_rom_addresss          : out std_logic_vector(31 downto 0);
-        i_current_instruction   : in  std_logic_vector(127 downto 0);
-        o_rom_ready             : out std_logic
-    );
+  port (
+    i_clock : in std_logic;
+    i_reset : in std_logic; -- synchronous, active-high
+    -- inputs
+    i_cu_start : in std_logic;
+    o_cu_done  : out std_logic;
+    --Program Counter inteface
+    i_program_count         : in std_logic_vector(31 downto 0);
+    o_program_counter_ready : out std_logic;
+    o_new_program_count     : out std_logic_vector(31 downto 0);
+    o_jump_flag             : out std_logic;
+    --Instruction memory interface
+    o_rom_address         : out std_logic_vector(31 downto 0);
+    i_current_instruction : in std_logic_vector(127 downto 0);
+    o_rom_ready           : out std_logic;
+    --Matrix Bank Interface
+    o_matrix_sel              : out integer range 0 to 3;
+    o_scalar_or_vector_action : out std_logic;
+    o_rw_vector               : out std_logic;
+    o_column_or_row_order     : out std_logic;
+    o_vector_i, o_vector_j    : out integer range 0 to VECTOR_SIZE - 1;
+    o_vector                  : out vector_t;
+    i_vector                  : in vector_t;
+    o_rw_scalar               : out std_logic;
+    o_scalar_i, o_scalar_j    : out integer range 0 to VECTOR_SIZE - 1;
+    o_scalar                  : out complex_t;
+    i_scalar                  : in complex_t
+  );
 end entity;
 
 architecture rtl of control_unit is
-    -- 1) State type
-    type   state_t               is (S_IDLE, S_FETCH, S_DECODE, S_EXECUTE, S_MEM, S_WRITEBACK, S_ERROR, S_DONE);
-    signal state, state_next     : state_t;
-    signal r_current_instruction : std_logic_vector(127 downto 0);
-    signal r_current_address : std_logic_vector(31 downto 0);
+  -- 1) State type
+  type state_t is (S_IDLE, S_FETCH, S_DECODE, S_EXECUTE, S_MEM, S_WRITEBACK, S_ERROR, S_DONE);
+  signal state, state_next     : state_t;
+  signal r_current_instruction : std_logic_vector(127 downto 0);
+  signal r_current_address     : std_logic_vector(31 downto 0);
 
 begin
 
-    --------------------------------------------------------------------
-    -- 2) State register, advances to next state and determines if reset is needed
-    --------------------------------------------------------------------
-    p_state : process(i_clock)
-    begin
-        if rising_edge(i_clock) then
-            if i_reset = '1' then
-                state <= S_IDLE;
-            else
-                state <= state_next;
-            end if;
+  --------------------------------------------------------------------
+  -- 2) State register, advances to next state and determines if reset is needed
+  --------------------------------------------------------------------
+  p_state : process (i_clock)
+  begin
+    if rising_edge(i_clock) then
+      if i_reset = '1' then
+        state <= S_IDLE;
+      else
+        state <= state_next;
+      end if;
+    end if;
+  end process;
+
+  --------------------------------------------------------------------
+  -- 2) Register, for normal registers and clocked behavior
+  --------------------------------------------------------------------
+
+  registers : process (i_clock) is
+  begin
+    if (rising_edge(i_clock)) then
+      case state is
+        when S_IDLE =>
+          null;
+        when S_FETCH =>
+          r_current_address <= i_program_count;
+        when S_DECODE =>
+          r_current_instruction <= i_current_instruction;
+          null;
+        when S_EXECUTE =>
+          null;
+        when S_MEM =>
+          null;
+        when S_WRITEBACK =>
+          null;
+        when S_ERROR =>
+          null;
+        when S_DONE =>
+          null;
+      end case;
+    end if;
+  end process registers;
+
+  --------------------------------------------------------------------
+  -- 3) Next-state 
+  --------------------------------------------------------------------
+  next_state_logic : process (state, i_cu_start, i_reset)
+  begin
+    state_next <= state;
+    case state is
+      when S_IDLE =>
+        if (i_cu_start = '1') then
+          state_next <= S_FETCH;
+        else
+          state_next <= S_IDLE;
         end if;
-    end process;
-
-    --------------------------------------------------------------------
-    -- 2) Register, for normal registers and clocked behavior
-    --------------------------------------------------------------------
-
-    registers : process(i_clock) is
-    begin
-        if (rising_edge(i_clock)) then
-            case state is
-                when S_IDLE =>
-                    null;
-                when S_FETCH =>
-                    r_current_address <= i_program_count;
-                when S_DECODE =>
-                    r_current_instruction <= i_current_instruction;
-                    null;
-                when S_EXECUTE =>
-                    null;
-                when S_MEM =>
-                    null;
-                when S_WRITEBACK =>
-                    null;
-                when S_ERROR =>
-                    null;
-                when S_DONE =>
-                    null;
-            end case;
+      when S_FETCH =>
+        if i_reset = '1' then
+          state_next <= S_IDLE;
         end if;
-    end process registers;
+        state_next <= S_DECODE;
+      when S_DECODE =>
+        if i_reset = '1' then
+          state_next <= S_IDLE;
+        end if;
+        state_next <= S_EXECUTE;
+      when S_EXECUTE =>
+        if i_reset = '1' then
+          state_next <= S_IDLE;
+        end if;
+        state_next <= S_MEM;
+      when S_MEM =>
+        if i_reset = '1' then
+          state_next <= S_IDLE;
+        end if;
+        state_next <= S_WRITEBACK;
+      when S_WRITEBACK =>
+        if i_reset = '1' then
+          state_next <= S_IDLE;
+        end if;
+        state_next <= S_FETCH;
+      when S_ERROR =>
+        if (i_reset = '0') then
+          state_next <= S_IDLE;
+        else
+          state_next <= S_ERROR;
+        end if;
+      when S_DONE =>
+        if (i_reset = '0') then
+          state_next <= S_IDLE;
+        else
+          state_next <= S_DONE;
+        end if;
+    end case;
 
-    --------------------------------------------------------------------
-    -- 3) Next-state 
-    --------------------------------------------------------------------
-    next_state_logic: process(state, i_cu_start, i_reset)
-    begin
-        state_next <= state;
-        case state is
-            when S_IDLE =>
-                if (i_cu_start = '1') then
-                    state_next <= S_FETCH;
-                else
-                    state_next <= S_IDLE;
-                end if;
-            when S_FETCH =>
-                if i_reset = '1' then
-                    state_next <= S_IDLE;
-                end if;
-                state_next <= S_DECODE;
-            when S_DECODE =>
-                if i_reset = '1' then
-                    state_next <= S_IDLE;
-                end if;
-                state_next <= S_EXECUTE;
-            when S_EXECUTE =>
-                if i_reset = '1' then
-                    state_next <= S_IDLE;
-                end if;
-                state_next <= S_MEM;
-            when S_MEM =>
-                if i_reset = '1' then
-                    state_next <= S_IDLE;
-                end if;
-                state_next <= S_WRITEBACK;
-            when S_WRITEBACK =>
-                if i_reset = '1' then
-                    state_next <= S_IDLE;
-                end if;
-                state_next <= S_FETCH;
-            when S_ERROR =>
-                if (i_reset = '0') then
-                    state_next <= S_IDLE;
-                else
-                    state_next <= S_ERROR;
-                end if;
-            when S_DONE =>
-                if (i_reset = '0') then
-                    state_next <= S_IDLE;
-                else
-                    state_next <= S_DONE;
-                end if;
-        end case;
+  end process;
 
-    end process;
-
-    output_logic: process is
-    begin
-        o_cu_done <= '0';
-        o_jump_flag <= '0';
-        o_new_program_count <= (others => '0');
-        o_program_counter_ready <= '0';
-        o_rom_addresss <= (others => '0');
-        o_rom_ready <= '0';
-        case state is
-            when S_IDLE =>
-                null;
-            when S_FETCH =>
-                o_rom_addresss <= i_program_count;
-            when S_DECODE =>
-                o_rom_addresss <= r_current_address;
-                null;
-            when S_EXECUTE =>
-                null;
-            when S_MEM =>
-                null;
-            when S_WRITEBACK =>
-                null;
-            when S_ERROR =>
-                null;
-            when S_DONE =>
-                null;
-        end case;
-    end process output_logic;
-    
-
+  output_logic : process (all) is
+  begin
+    o_cu_done               <= '0';
+    o_jump_flag             <= '0';
+    o_new_program_count     <= (others => '0');
+    o_program_counter_ready <= '0';
+    o_rom_address           <= (others => '0');
+    o_rom_ready             <= '0';
+    case state is
+      when S_IDLE =>
+        null;
+      when S_FETCH =>
+        o_rom_address <= i_program_count;
+      when S_DECODE =>
+        o_rom_address <= r_current_address;
+        null;
+      when S_EXECUTE =>
+        null;
+      when S_MEM =>
+        null;
+      when S_WRITEBACK =>
+        null;
+      when S_ERROR =>
+        null;
+      when S_DONE =>
+        null;
+    end case;
+  end process output_logic;
 end architecture;
--- tb/tb_lapu.vhd
+++ tb/tb_lapu.vhd
@@ -1,31 +1,34 @@
--- tb_top.vhd — add/sub only (clean, wave-friendly)
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use std.env.all;
-
-library fixed_pkg;
-use fixed_pkg.fixed_pkg.all;
+use ieee.fixed_pkg.all;
 
 library tensors;
 use tensors.tensors.all;
 
+library std;
+use std.env.all;
 entity tb_lapu is
 end entity;
 
 architecture sim of tb_lapu is
 
-  -- Tiny clock so waveform viewers always get timestamps
-  signal w_clock, w_reset, w_cu_start, w_done, w_program_count_ready, w_rom_ready : std_logic                        := '0';
-  signal w_jump_flag                                                              : std_logic                        := '0';
-  signal w_program_count, w_new_program_count, w_rom_address                      : std_logic_vector(31 downto 0)    := (others => '0');
-  signal w_instruction                                                            : std_logic_vector((127) downto 0) := (others => '0');
+  signal stop_sim : std_logic := '0';
 
+  signal w_clock, w_reset, w_cu_start, w_done, w_program_count_ready, w_rom_ready, w_scalar_or_vector_action, w_rw_vector, w_column_or_row_order, w_rw_scalar : std_logic                          := '0';
+  signal w_jump_flag                                                                                                                                          : std_logic                          := '0';
+  signal w_program_count, w_new_program_count, w_rom_address                                                                                                  : std_logic_vector(31 downto 0)      := (others => '0');
+  signal w_instruction                                                                                                                                        : std_logic_vector((127) downto 0)   := (others => '0');
+  signal w_matrix_sel                                                                                                                                         : integer range 0 to 3               := 0;
+  signal w_vector_i, w_vector_j, w_scalar_i, w_scalar_j                                                                                                       : integer range 0 to (VECTOR_SIZE - 1) := 0;
+  signal w_in_vector, w_out_vector                                                                                                                            : vector_t                           := VECTOR_ZERO;
+  signal w_in_scalar, w_out_scalar                                                                                                                            : complex_t                          := COMPLEX_ZERO;
 begin
   w_clock <= not w_clock after 1 ns;
 
   program_counter_inst : entity work.program_counter
-    port map(
+    port map
+    (
       i_clock             => w_clock,
       i_reset             => w_reset,
       i_pc_ready          => w_program_count_ready,
@@ -35,38 +38,73 @@ begin
     );
 
   control_unit_inst : entity work.control_unit
-    port map(
-      i_clock                 => w_clock,
-      i_reset                 => w_reset,
-      i_cu_start              => w_cu_start,
-      o_cu_done               => w_done,
-      i_program_count         => w_program_count,
-      o_program_counter_ready => w_program_count_ready,
-      o_new_program_count     => w_new_program_count,
-      o_jump_flag             => w_jump_flag,
-      i_current_instruction   => w_instruction
+    port map
+    (
+      i_clock                   => w_clock,
+      i_reset                   => w_reset,
+      i_cu_start                => w_cu_start,
+      o_cu_done                 => w_done,
+      i_program_count           => w_program_count,
+      o_program_counter_ready   => w_program_count_ready,
+      o_new_program_count       => w_new_program_count,
+      o_jump_flag               => w_jump_flag,
+      o_rom_address             => w_rom_address,
+      i_current_instruction     => w_instruction,
+      o_rom_ready               => w_rom_ready,
+      o_matrix_sel              => w_matrix_sel,
+      o_scalar_or_vector_action => w_scalar_or_vector_action,
+      o_rw_vector               => w_rw_vector,
+      o_column_or_row_order     => w_column_or_row_order,
+      o_vector_i                => w_vector_i,
+      o_vector_j                => w_vector_j,
+      o_vector                  => w_out_vector,
+      i_vector                  => w_in_vector,
+      o_rw_scalar               => w_rw_scalar,
+      o_scalar_i                => w_scalar_i,
+      o_scalar_j                => w_scalar_j,
+      o_scalar                  => w_out_scalar,
+      i_scalar                  => w_in_scalar
     );
-
   instruction_memory_inst : entity work.instruction_memory
     generic map(
       MAX_ADDRESS      => 256,
       INSTRUCTION_SIZE => 128
     )
-    port map(
+    port map
+    (
       i_clock       => w_clock,
       i_rom_ready   => w_rom_ready,
       i_rom_address => w_rom_address,
       o_instruction => w_instruction
     );
 
+  matrix_bank_inst : entity work.matrix_bank
+    port map
+    (
+      i_clock                   => w_clock,
+      i_reset                   => w_reset,
+      i_matrix_sel              => w_matrix_sel,
+      i_scalar_or_vector_action => w_scalar_or_vector_action,
+      i_rw_vector               => w_rw_vector,
+      i_column_or_row_order     => w_column_or_row_order,
+      i_vector_i                => w_vector_i,
+      i_vector_j                => w_vector_j,
+      i_vector                  => w_out_vector,
+      o_vector                  => w_in_vector,
+      i_rw_scalar               => w_rw_scalar,
+      i_scalar_i                => w_scalar_i,
+      i_scalar_j                => w_scalar_j,
+      i_scalar                  => w_out_scalar,
+      o_scalar                  => w_in_scalar
+    );
   stim : process
   begin
     -- Wait past t=0 so changes land at real time
-    w_reset    <= '0';
+    w_reset <= '0';
     wait for 100 ns;
     w_cu_start <= '1';
     wait for 100 ns;
-    stop;
+    stop_sim <= '1';
     wait;
   end process;
 end architecture sim;
